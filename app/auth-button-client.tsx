2f35b6ead59473079624f2caac44b9890ef828c2
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-08-31 16:54:20 +0200

    auth-button-client.tsx redirectTo https://nextwitter-chi.vercel.app/auth/callback

--- app/auth-button-client.tsx
+++ app/auth-button-client.tsx
@@ -11,7 +11,7 @@ export function AuthButtonClient ({ session }: { session: Session | null }) {
   async function handleSingIn () {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
-      options: { redirectTo: 'http://localhost:3000/auth/callback' }
+      options: { redirectTo: 'https://nextwitter-chi.vercel.app/auth/callback' }
     })
   }
   async function handleSingOut () {
